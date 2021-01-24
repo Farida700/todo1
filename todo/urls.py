95c37ddc683712b63b6ba59a20f541c840d18d19
Фарида <happy_w@bk.ru>
2021-01-24 14:30:03 +0600

    file

--- todo/urls.py
+++ todo/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path("test/", test, name="Test"),
     path("test2/", second),
     path("test3/", third)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+
